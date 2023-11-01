3f3f72808cc39f50a4fd6f17a0dee2c036700e61
Catalina Popa <catalinaelenapopa07@gmail.com>
2023-11-01 14:14:26 +0200

    Extended the main based on the project requirements

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
+from enum import Enum
 import pandas as pd
 
 
-# -------------------------------------------DATA CLASS--------------------------------------------
+# ----------------------------------------------DATA-----------------------------------------------
+# Class for all types of data: input CSVs, best trees generated, etc.
+# Best trees generated - may be a priority queue (ordered by tree's score)
 class Data:
     results_df = None
     ground_truth_df = None
@@ -14,6 +17,42 @@ class Data:
     def read_csv_files(self, res_file, gnd_file):
         self.results_df = pd.read_csv(res_file)
         self.ground_truth_df = pd.read_csv(gnd_file)
+
+
+# ---------------------------------------------NODE------------------------------------------------
+# Class for tree node that stores: value, tree_level, node_index, assigned_op, is_leaf
+# <value>: threshold - leaves; computed result - other nodes
+# <tree_level>: the level in the tree
+# <node_index>: the node's index on the specified level
+# <assigned_op>: None - leaves; randomly chosen op - other nodes
+# <is_leaf>: boolean flag that checks if the node is a flag or not
+
+# TODO: implement class Node
+class Node:
+    pass
+
+
+# ---------------------------------------------TREE------------------------------------------------
+class Tree:
+    def __init__(self):
+        self.generate_tree()
+
+    # TODO: Randomly generate number of levels, number of nodes per level, operations for nodes, etc..
+    def generate_tree(self):
+        pass
+
+
+# -------------------------------------------OPERATIONS--------------------------------------------
+# Class for all mathematical operations that tree nodes can be randomly chosen from
+class Operations(Enum):
+    # TODO: add all possible mathematical operations - examples in the project's text
+    MIN = 1
+    MAX = 2
+
+    def __init__(self):
+        super().__init__()
+
+    # TODO: add methods that can handle 2 or more inputs for each operation defined above
 
 
 # ---------------------------------------------UTILS-----------------------------------------------
@@ -29,11 +68,10 @@ def create_human_readable_inputs(data):
 
 if __name__ == "__main__":
     # When running from the command-line
-    # _, algo_results_file, ground_truth_file = sys.argv
+    # _, results_file, ground_truth_file = sys.argv
 
     results_file = 'input/train_algo-results.csv'
     ground_truth_file = 'input/train_ground-truth.csv'
 
     data_obj = Data(results_file, ground_truth_file)
     create_human_readable_inputs(data_obj)
-
